dbd3499b2ecaad8aa8df0afdfedb182d3e68cf9c
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-03 12:07:54 +0200

    WIP: test analysis 1 station

--- wifi_stats/setup.sh
+++ wifi_stats/setup.sh
@@ -29,20 +29,20 @@ done
 echo  ------------------------------------------------
 # Generate random files
 echo Running program: GENERATE RANDOM FILES
-# python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_1/stations.yml -lc $LOGGING_CONFIG_FILE
+python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_1/stations.yml -lc $LOGGING_CONFIG_FILE
 # python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_2/stations.yml -lc $LOGGING_CONFIG_FILE
 # python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_3/stations.yml -lc $LOGGING_CONFIG_FILE
 # python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_4/stations.yml -lc $LOGGING_CONFIG_FILE
-python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_5/stations.yml -lc $LOGGING_CONFIG_FILE
+# python3 main.py -p generate_random_files -st $TOTAL_NUMBER_OF_FILES_TO_TRANSFER -sd $TRANSFER_STEP_DURATION_IN_SECS -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_5/stations.yml -lc $LOGGING_CONFIG_FILE
 echo  ------------------------------------------------
 
 # Send transfer files to connected stations
 
 echo Transfering files to stations
-# python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_1/stations.yml -lc $LOGGING_CONFIG_FILE
+python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_1/stations.yml -lc $LOGGING_CONFIG_FILE
 # python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_2/stations.yml -lc $LOGGING_CONFIG_FILE
 # python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_3/stations.yml -lc $LOGGING_CONFIG_FILE
 # python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_4/stations.yml -lc $LOGGING_CONFIG_FILE
-python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_5/stations.yml -lc $LOGGING_CONFIG_FILE
+# python3 main.py -p initial_files_transfer_to_stations -fc $SETUP_FILES_TRASNSFER_CONFIG_DIR/stations_5/stations.yml -lc $LOGGING_CONFIG_FILE
 echo  ------------------------------------------------
 
